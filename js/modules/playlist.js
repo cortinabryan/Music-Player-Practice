993242a6e249937a44ba43baff0dcc6b085f9ec4
unknown <cortinabryan@gmail.com>
2021-03-14 09:00:23 -0700

    Updated track-bar component

--- js/modules/playlist.js
+++ js/modules/playlist.js
@@ -1,5 +1,6 @@
 import { songsList } from "../data/songs.js";
 import PlayInfo from "./play-info.js";
+import TrackBar from "./track-bar.js";
 
 const Playlist = ((_) => {
   //data or state
@@ -70,6 +71,10 @@ const Playlist = ((_) => {
         mainPlay(listElemIndex);
         render();
       }
+    });
+
+    currentSong.addEventListener("timeupdate", (_) => {
+      TrackBar.setState(currentSong);
     });
 
     currentSong.addEventListener("ended", (_) => {
